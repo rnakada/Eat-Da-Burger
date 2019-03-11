22bc0cc39ee6a093223ae099e16cdd021fd640d7
unknown <rnakada90@gmail.com>
2019-03-10 21:13:40 -0400

    jawDB

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,23 @@
 // Setup mysql
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+
+};
 
 // Make connection
 connection.connect((err) => {
-    if(err) {
+    if (err) {
         console.error("error connecting: " + err.stack);
         return;
     }
